fc1f565188b483391a776e6b2e1b2ea33bf2c05e
Kieran Reid <kieranreid2612@gmail.com>
2024-09-06 12:28:26 +0100

    Initial Commit

---
+++ spotify_clone/spotify_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotify_cloneApp.swift
+//  spotify_clone
+//
+//  Created by Kieran Reid on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct spotify_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
