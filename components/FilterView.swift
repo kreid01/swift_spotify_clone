b9e687cc67eeb033af21f718e59236a39440c2b6
Kieran Reid <kieranreid2612@gmail.com>
2024-09-11 17:52:29 +0100

    feat: Library view

--- components/FilterView.swift
+++ components/FilterView.swift
@@ -2,11 +2,7 @@ import SwiftUI
 
 struct FilterView: View {
     var mediaFilters = ["All", "Music", "Podcasts", "Audiobooks", "Courses"]
-    @State var selectedMediaFilter = "All"
-
-    var gridLayout: [GridItem] = [
-        GridItem(.fixed(110)),
-    ]
+    var selectedMediaFilter = "All"
 
     var body: some View {
         HStack {
@@ -14,24 +10,38 @@ struct FilterView: View {
             Circle()
                 .frame(width: 32, height: 32)
                 .foregroundStyle(.white)
-            ScrollView(.horizontal) {
-                LazyHGrid(rows: gridLayout) {
-                    ForEach(mediaFilters, id: \.self) {
-                        media in
-                        Text(media)
-                            .padding(.horizontal, 12)
-                            .padding(.vertical, 7)
-                            .background(selectedMediaFilter == media ? .green : Color(red: 41/255, green: 41/255, blue: 41/255))
-                            .foregroundStyle(.white)
-                            .cornerRadius(20)
-                            .padding(4)
-                            .onTapGesture {
-                                selectedMediaFilter = media
-                            }
-                    }
-                }
-            }.scrollIndicators(.hidden)
+            Filters(mediaFilters: mediaFilters, selectedMediaFilter: selectedMediaFilter)
         }.frame(height: 70)
             .padding(.leading, 15)
+    }
+}
+
+struct Filters: View {
+    @State var mediaFilters: [String]
+    @State var selectedMediaFilter: String
+
+    var gridLayout: [GridItem] = [
+        GridItem(.fixed(110)),
+    ]
+
+    var body: some View {
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: gridLayout) {
+                ForEach(mediaFilters, id: \.self) {
+                    media in
+                    Text(media)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 7)
+                        .background(selectedMediaFilter == media ? .green : Color(red: 41/255, green: 41/255, blue: 41/255))
+                        .foregroundStyle(.white)
+                        .cornerRadius(20)
+                        .padding(4)
+                        .onTapGesture {
+                            selectedMediaFilter = media
+                        }
+                }
+            }
+        }.scrollIndicators(.hidden)
+            .frame(height: 70)
     }
 }
---
+++ views/LibraryView.swift
@@ -0,0 +1,66 @@
+import SwiftUI
+
+struct LibraryView: View {
+    var gridLayout: [GridItem] = [
+        GridItem(.fixed(120)),
+        GridItem(.fixed(120)),
+        GridItem(.fixed(120)),
+    ]
+
+    var mediaFilters = ["Playlists", "Podcasts and courses", "Albums", "Artists", "Downloaded"]
+    var selectedMediaFilter = ""
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack {
+                    Circle()
+                        .frame(width: 32, height: 32)
+                        .padding(.leading, 20)
+                        .foregroundStyle(.white)
+                    Text("Your Library").foregroundStyle(.white)
+                        .fontWeight(.bold)
+                        .textInputAutocapitalization(.never)
+                        .keyboardType(.asciiCapable)
+                        .font(.system(size: 24))
+                    Spacer()
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.white)
+                        .padding(.trailing, 20)
+                    Image(systemName: "plus")
+                        .foregroundColor(.white)
+                        .padding(.trailing, 20)
+                }
+
+                Filters(mediaFilters: mediaFilters, selectedMediaFilter: selectedMediaFilter)
+                    .padding(.leading, 10)
+
+                ScrollView {
+                    LazyVGrid(columns: gridLayout) {
+                        ForEach(0 ..< 20) { _ in
+                            VStack {
+                                HStack {}
+                                    .frame(width: 110, height: 110)
+                                    .background(.white)
+                                    .cornerRadius(5)
+                                Text("sacue")
+                                    .foregroundStyle(.white)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .offset(x: 5)
+                                Text("sacue")
+                                    .foregroundStyle(.gray)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .offset(x: 5, y: -5)
+                            }
+                        }
+                    }
+                }
+            }
+            .background(Color(red: 25/255, green: 25/255, blue: 25/255))
+        }
+    }
+}
+
+#Preview {
+    LibraryView()
+}
